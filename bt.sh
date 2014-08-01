dda852a186cfd013c492445bc7f53c21582433d2
Kaspter Ju <nigh0st3018@gmail.com>
2014-08-02 01:47:57 +0800

    meson: simple build test script bt.sh

---
+++ bt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#------------IMPORTANT------------#
+#--RUN THIS SCRIPT BEFOR COMMIT---#
+#---------------------------------#
+
+#usage:
+#
+
+
+make distclean && make m8_k200_v1_config && make -j8
+
+make distclean && make m6_mbx_v2_config && make -j8
