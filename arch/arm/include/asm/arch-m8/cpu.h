a08ddaae8ecb6f9330f99f6f27fce9ce11855ae3
Kaspter Ju <nigh0st3018@gmail.com>
2015-05-01 00:07:21 +0800

    meson: clean RTC

--- arch/arm/include/asm/arch-m8/cpu.h
+++ arch/arm/include/asm/arch-m8/cpu.h
@@ -255,9 +255,6 @@
 #define CONFIG_CMD_KGDB			1
 ////#define CONFIG_SERIAL_TAG       1*/
 
-//#define CONFIG_AML_RTC
-//#define CONFIG_RTC_DAY_TEST 1  // test RTC run 2 days
-
 #define CONFIG_LZMA  1
 #define CONFIG_LZO
 /*default command select*/
