7914d13431b94d9fceec607e76088c5e1e5da528
Kaspter Ju <kaspter@kmatt.net>
2017-11-16 16:52:24 +0800

    RTAVS: board: add menus rk3288 board
    CPU: RK3288 + DDR3(2G) + EMMC(8G) + HDMI + ACT8846 + AP6181
    
    Change-Id: I74051ede70912afdf25914d52c3d87b3a7dfe7c3
    Signed-off-by: Kaspter Ju <kaspter@kmatt.net>

---
+++ include/configs/menus_rk3288.h
@@ -0,0 +1,23 @@
+/*
+ * (C) Copyright 2015 Google, Inc
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdin=serial\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3288_common.h>
+
+#define CONFIG_SYS_MMC_ENV_DEV 0
+
+#define CONFIG_MISC_INIT_R
+#define CONFIG_SERIAL_TAG
+#define CONFIG_ENV_OVERWRITE
+
+#endif
